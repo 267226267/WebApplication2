6a8c10110f5fb5b3efc1319956a4dcdd117d511a
zjhkf123 <12850720+zjhkf123@user.noreply.gitee.com>
2023-04-27 07:35:27 +0800

    edit v2

--- WebApplication2/Person.cs
+++ WebApplication2/Person.cs
@@ -7,10 +7,26 @@ namespace WebApplication2
 {
     public class Person
     {
-        public Person() { 
-        
+        public Person(string username) {
+            this.UserName = username;
+            this.Age = 0;
         }
         public string UserName { get; set; }
         public int Age { get; set; }
+        public decimal salary { get; set; }
+        public int GetAge()
+        {
+            return Age;
+        }
+        public void M1()
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+
+        }
+        public void M2()
+        {
+
+        }
     }
 }
