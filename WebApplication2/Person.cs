560a4e42ea68dd3a56489016871b833ae5995b81
zjhkf123 <12850720+zjhkf123@user.noreply.gitee.com>
2023-04-27 07:33:30 +0800

    初始化项目

---
+++ WebApplication2/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Person
+    {
+        public Person() { 
+        
+        }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+    }
+}
